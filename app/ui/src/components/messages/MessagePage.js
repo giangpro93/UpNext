75bcce93d971ef9f299bad580c6bd9b0f3bbba2a
jac-mac <jackrmcclure@gmail.com>
2021-03-22 11:49:58 -0500

    Setup environment

--- app/ui/src/components/messages/MessagePage.js
+++ app/ui/src/components/messages/MessagePage.js
@@ -1,4 +1,14 @@
-import React, { Component }, ReactDOM from 'react';
-const title = <h1>Messages</h1>;
+import React, { useState, useEffect } from 'react';
+import SearchBar from "material-ui-search-bar";
 
-ReactDOM.render(title, document.getElementById('root'));
+export function MessagePage() {
+
+  return (
+    <SearchBar
+      value={this.state.value}
+      onChange={(newValue) => this.setState({ value: newValue })}
+      onRequestSearch={() => doSomethingWith(this.state.value)}
+      />
+  );
+}
+export default MessagePage;
