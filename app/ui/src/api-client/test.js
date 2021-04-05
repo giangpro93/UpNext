339c2a87c2c7ae0594d86ec8127b946a58f09fc8
Jace Kline <jace.a.kline@gmail.com>
2021-04-05 16:25:13 -0500

    Fixed makeAdmin function

--- app/ui/src/api-client/test.js
+++ app/ui/src/api-client/test.js
@@ -3,6 +3,6 @@ const { axiosInstance } = require('./api-client');
 
 const reqObj = {user_id: 1, group_id: 11};
 
-api.memberships.deleteMembership(reqObj)
+api.memberships.makeAdmin(reqObj)
 .then(console.log)
 .catch(console.log)
