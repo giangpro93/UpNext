177940781495080cd7befeac502dcb12e7685fc5
Jace Kline <jace.a.kline@gmail.com>
2021-04-10 14:50:24 -0500

    Profile card

--- app/ui/src/components/profile-page/ProfileCard.js
+++ app/ui/src/components/profile-page/ProfileCard.js
@@ -1,8 +1,9 @@
-import { Card, CardActions, CardContent, Typography } from '@material-ui/core';
+import { Card, CardActions, CardContent, CardHeader, Typography } from '@material-ui/core';
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Input } from '../common/Input';
 import EditProfileForm from './EditProfileForm';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 export default function ProfileCard(props) {
     const { user } = props;
@@ -12,8 +13,12 @@ export default function ProfileCard(props) {
     const isMe = user.id === currentUser.id;
     return (
         <>
-        <Card raised variant='outlined'>
+        <Card raised variant='outlined' style={{display: 'inline-block'}}>
             <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    Profile Info
+                </Typography>
+                <AccountCircleIcon />
                 <Typography>
                     Name: {user.name}
                 </Typography>
